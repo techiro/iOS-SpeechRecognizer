ec27d1c64eefc1858eac8c8305de1070a3bff310
techiro <techiromen0@gmail.com>
2020-08-06 14:53:07 +0900

    Initial Commit

---
+++ iOS-SpeechRecognizer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS-SpeechRecognizer
+//
+//  Created by TanakaHirokazu on 2020/08/06.
+//  Copyright © 2020 TanakaHirokazu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
