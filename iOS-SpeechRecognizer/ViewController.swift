5b8af6e52bad4b578b44fd26ef2eab5741f773e7
techiro <techiromen0@gmail.com>
2020-08-08 15:07:24 +0900

    UIパーツの設置

--- iOS-SpeechRecognizer/ViewController.swift
+++ iOS-SpeechRecognizer/ViewController.swift
@@ -10,11 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textView: UITextView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func recordButtonTapped(_ sender: Any) {
+        
+    }
+    
 }
 
