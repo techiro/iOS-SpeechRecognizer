70302fa589edcfe91ab96994703e624228d69d2d
techiro <techiromen0@gmail.com>
2020-08-08 23:13:05 +0900

    音声認識実装

--- README.md
+++ README.md
@@ -32,3 +32,125 @@ SFSpeechRecognizerの生成
 
 
 
+## textView
+
+UITextViewを配置（認識された音声をテキスト表示: textView）
+背景・テキストカラー・フォントサイズを変更
+インスペクタエリアから*isUserInteractionプロパティ*を無効にする
+
+
+## recognitionRequest
+録音完了前に途中の結果を報告してくれる（デフォルトはfalse）
+```swift
+recognitionRequest.shouldReportPartialResults = true
+
+speechRecognizer.delegate = self
+
+ recognitionRequest?.endAudio()
+ 
+```
+###デリゲート処理
+ViewControllerクラスにデリゲートを採用する
+speechRecognizerの状態変化を受け取れるようになる
+`SFSpeechRecognitionTaskDelegate`
+
+```swift
+
+// MARK: SFSpeechRecognizerDelegate
+//speechRecognizerが使用可能かどうかでボタンのisEnabledを変更する
+public func speechRecognizer(_ speechRecognizer: SFSpeechRecognizer, availabilityDidChange available: Bool) {
+    if available {
+        recordButton.isEnabled = true
+        recordButton.setTitle("Start Recording", for: [])
+        
+    } else {
+        recordButton.isEnabled = false
+        recordButton.setTitle("Recognition not available", for: .disabled)
+    }
+}
+```
+## オーディオセッションの設定
+
+let audioSession = AVAudioSession.sharedInstance()
+try audioSession.setCategory(.record, mode: .measurement, options: .duckOthers)
+try audioSession.setActive(true, options: .notifyOthersOnDeactivation)
+let inputNode = audioEngine.inputNode
+
+## 端末のマイクを使う準備
+
+
+
+##RecordButtonの実装
+
+```swift
+
+@IBAction func recordButtonTapped() {
+    if audioEngine.isRunning {
+    // 音声エンジン動作中なら停止
+        audioEngine.stop()
+        recognitionRequest?.endAudio()
+        recordButton.isEnabled = false
+        recordButton.setTitle("Stopping", for: .disabled)
+        recordButton.backgroundColor = UIColor.lightGray
+        return
+    }
+    // 録音を開始する
+    try! startRecording()
+    recordButton.setTitle("認識を完了する", for: [])
+    recordButton.backgroundColor = UIColor.red
+}
+
+```
+
+
+```swift
+
+private func speak(message: String) {
+       defer {
+           disableAVSession()
+       }
+       do {
+           try AVAudioSession.sharedInstance().setCategory(.playAndRecord, mode: .default, options: .defaultToSpeaker)
+           try AVAudioSession.sharedInstance().setActive(true, options: .notifyOthersOnDeactivation)
+       } catch {
+           print("audioSession properties weren't set because of an error.")
+       }
+       let utterance = AVSpeechUtterance(string: message)
+       utterance.voice = AVSpeechSynthesisVoice(language: "ja-JP")
+       utterance.pitchMultiplier = 1
+       self.speechSynthesizer.speak(utterance)
+   }
+   
+   private func disableAVSession() {
+       do {
+           try AVAudioSession.sharedInstance().setActive(false, options: .notifyOthersOnDeactivation)
+       } catch {
+           print("audioSession properties weren't disable.")
+       }
+   }
+
+```
+
+
+```swift
+
+private func requestRecognizerAuthorization() {
+    SFSpeechRecognizer.requestAuthorization { authStatus in
+        // メインスレッドで処理したい内容のため、OperationQueue.main.addOperationを使う
+        OperationQueue.main.addOperation { [weak self] in
+            guard let `self` = self else { return }
+            switch authStatus {
+            case .authorized:
+                self.textView.text = "音声認識へのアクセスが許可されています。"
+            case .denied:
+                self.textView.text = "音声認識へのアクセスが拒否されています。"
+            case .restricted:
+                self.textView.text = "音声認識へのアクセスが制限されています。"
+            case .notDetermined:
+                self.textView.text = "音声認識はまだ許可されていません。"
+            }
+        }
+    }
+}
+
+```
--- iOS-SpeechRecognizer/ViewController.swift
+++ iOS-SpeechRecognizer/ViewController.swift
@@ -15,13 +15,15 @@ class ViewController: UIViewController {
     var audioEngine: AVAudioEngine!
     var recognitionRequest: SFSpeechAudioBufferRecognitionRequest?
     var recognitionTask: SFSpeechRecognitionTask?
-    
+    var isRecord = false
     @IBOutlet weak var textView: UITextView!
     @IBOutlet weak var recordButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         audioEngine = AVAudioEngine()
+        recognizer.delegate = self
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -34,8 +36,92 @@ class ViewController: UIViewController {
     }
     
     @IBAction func recordButtonTapped(_ sender: Any) {
-        
+        if isRecord {
+            stopRecognition()
+            isRecord = false
+        } else {
+            try! startRecognition()
+            isRecord = true
+        }
     }
     
+    
+    func stopRecognition() {
+      audioEngine.stop()
+      audioEngine.inputNode.removeTap(onBus: 0)
+      recognitionRequest?.endAudio()
+      recordButton.setTitle("Start", for: .normal)
+    }
+    
+    private func refreshTask() {
+        if let recognitionTask = recognitionTask {
+            recognitionTask.cancel()
+            self.recognitionTask = nil
+        }
+    }
+    
+    func startRecognition() throws {
+        
+        refreshTask()
+
+        let audioSession = AVAudioSession.sharedInstance()
+        try audioSession.setCategory(.record, mode: .measurement, options: [])
+        try audioSession.setActive(true, options: .notifyOthersOnDeactivation)
+
+        recognitionRequest = SFSpeechAudioBufferRecognitionRequest()
+        
+        guard let recognitionRequest = recognitionRequest else { fatalError("Unable to created a SFSpeechAudioBufferRecognitionRequest object") }
+        guard let inputNode = audioEngine?.inputNode else { fatalError("Audio engine has no input node") }
+        
+        recognitionRequest.shouldReportPartialResults = true
+        recognitionTask = recognizer.recognitionTask(with: recognitionRequest) { [weak self] (result, error) in
+            guard let self = self else {
+                return
+            }
+            
+            var isFinal = false
+            if let result = result {
+                isFinal = result.isFinal
+                self.textView.text = result.bestTranscription.formattedString
+            }
+
+            if error != nil || isFinal {
+                //エラーかisFinalフラグが立つと終了
+                self.audioEngine.stop()
+                inputNode.removeTap(onBus: 0)
+                self.recognitionRequest = nil
+                self.recognitionTask = nil
+            }
+    }
+        
+        let recordingFormat = inputNode.outputFormat(forBus: 0)
+        inputNode.installTap(onBus: 0, bufferSize: 1024, format: recordingFormat) { (buffer: AVAudioPCMBuffer, when: AVAudioTime) in
+            self.recognitionRequest?.append(buffer)
+        }
+
+        try startAudioEngine()
+    }
+    
+    private func startAudioEngine() throws {
+        audioEngine.prepare()
+        try audioEngine.start()
+        recordButton.setTitle("Stop", for: .normal)
+    }
 }
 
+
+extension ViewController: SFSpeechRecognizerDelegate {
+    func speechRecognizer(_ speechRecognizer: SFSpeechRecognizer, availabilityDidChange available: Bool) {
+        
+        if available {
+            recordButton.isEnabled = true
+            print("recognizer is available")
+        }else {
+            isRecord = false
+            recordButton.isEnabled = false
+            print("recognizer is not available")
+        }
+    }
+
+
+}
