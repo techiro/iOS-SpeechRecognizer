975b63819681274a8674cb4117ff9ec41e3325ff
techiro <techiromen0@gmail.com>
2020-08-08 15:15:55 +0900

    SFSpeechRecognizer.requestAuthorization メソッドでプライバシーの確認

--- iOS-SpeechRecognizer/ViewController.swift
+++ iOS-SpeechRecognizer/ViewController.swift
@@ -7,15 +7,32 @@
 //
 
 import UIKit
+import Speech
 
 class ViewController: UIViewController {
-
+    
+    let recognizer = SFSpeechRecognizer(locale: Locale.init(identifier: "ja_JP"))!
+    var audioEngine: AVAudioEngine!
+    var recognitionRequest: SFSpeechAudioBufferRecognitionRequest?
+    var recognitionTask: SFSpeechRecognitionTask?
+    
     @IBOutlet weak var textView: UITextView!
+    @IBOutlet weak var recordButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        audioEngine = AVAudioEngine()
     }
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        SFSpeechRecognizer.requestAuthorization { (status) in
+            if status != .authorized{
+                self.recordButton.isEnabled = false
+                self.recordButton.setTitle("Can't record", for: .normal)
+            }
+        }
+    }
+    
     @IBAction func recordButtonTapped(_ sender: Any) {
         
     }
